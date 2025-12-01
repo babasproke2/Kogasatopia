956a01cbad0372f3bf61dbde39b209830413946d
babasproke2 <xxlibtardeaterxx@gmail.com>
2025-12-01 15:10:53 -0500

    Update start scripts

--- tf2.sh
+++ tf2.sh
@@ -4,7 +4,7 @@ cd /home/kogasa/hlserver/tf2 || exit 1
 ./srcds_run -console -game tf \
     -port 27015 \
     -secure \
-    +map koth_eientei_final1 \
+    +map koth_product_pro \
     -autoupdate \
     -steam_dir /home/kogasa/hlserver \
     -steamcmd_script /home/kogasa/hlserver/update_script.txt \
